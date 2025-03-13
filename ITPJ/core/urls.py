c6a4f5ff6b788b9742cbcd74b475d4321be5b966
HE <2890237931@qq.com>
2025-03-13 13:01:14 +0000

    recon

--- ITPJ/core/urls.py
+++ ITPJ/core/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 
-from core.views import HomePageView, PostDetailView
+from core.views import HomePageView, PostDetailView, HomePageEquipmentView
 
 urlpatterns = [
     path('', HomePageView.as_view(), name='home'),
     path('post/<int:pk>/', PostDetailView.as_view(), name='post_detail'),
+    path('equipment/', HomePageEquipmentView.as_view(), name='equipment'),
+
 
 ]
--- ITPJ/core/views.py
+++ ITPJ/core/views.py
@@ -4,6 +4,8 @@ from django.http import JsonResponse
 from django.shortcuts import redirect, get_object_or_404
 from django.views.generic import TemplateView, DetailView
 from django.views import View
+from sympy.integrals.meijerint_doc import category
+from django.db.models import Q
 from core.models import Post
 
 logger = logging.getLogger(__name__)
@@ -18,6 +20,38 @@ class HomePageView(TemplateView):
         # need some sql or if-then to select posts to show
         return context
 
+class HomePageEquipmentView(TemplateView):
+    template_name = 'core/homepage.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['posts'] = Post.objects.filter(Q(category1=1)|Q(category2=1)|Q(category3=1)|Q(category4=1)|Q(category5=1)).order_by('-created_at')
+        # need some sql or if-then to select posts to show
+
+        # context['posts'] = Post.objects.filter(status='published').annotate(
+        #     score=F('likes') - F('dislikes')  # 计算得分
+        # ).order_by('-score', '-created_at')  # 先按得分降序，再按发布时间降序
+        return context
+#
+# class HomePageView(TemplateView):
+#     template_name = 'core/homepage.html'
+#
+#     def get_context_data(self, **kwargs):
+#         context = super().get_context_data(**kwargs)
+#         context['posts'] = Post.objects.all().order_by('-created_at')
+#         # need some sql or if-then to select posts to show
+#         return context
+#
+# class HomePageView(TemplateView):
+#     template_name = 'core/homepage.html'
+#
+#     def get_context_data(self, **kwargs):
+#         context = super().get_context_data(**kwargs)
+#         context['posts'] = Post.objects.all().order_by('-created_at')
+#         # need some sql or if-then to select posts to show
+#         return context
+#
+#
 
 
 
