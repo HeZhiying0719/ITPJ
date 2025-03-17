507be90a69a037475512d6a78444b979e55496fb
HE <2890237931@qq.com>
2025-03-17 17:31:09 +0000

    about-us, homepage

--- ITPJ/core/urls.py
+++ ITPJ/core/urls.py
@@ -1,10 +1,20 @@
 from django.urls import path
+from win32ui import CreateView
 
-from core.views import HomePageView, HomePageEquipmentView
+from core.views import *
+
 
 urlpatterns = [
     path('', HomePageView.as_view(), name='home'),
     path('equipment/', HomePageEquipmentView.as_view(), name='equipment'),
-
+    path('landscape/', HomeLandscapeView.as_view(), name='landscape'),
+    path('nature/', HomeNatureView.as_view(), name='nature'),
+    path('street/', HomeStreetView.as_view(), name='street'),
+    path('travel/', HomeTravelView.as_view(), name='travel'),
+    path('lifestyle/', HomeLifestyleView.as_view(), name='lifestyle'),
+    path('creative/', HomeCreativeView.as_view(), name='creative'),
+    path('object/', HomeObjectView.as_view(), name='object'),
+    path('portraiture/', HomePortraitureView.as_view(), name='portraiture'),
+    path('aboutus/', AboutUsView.as_view(), name='aboutus')
 
 ]
--- ITPJ/core/views.py
+++ ITPJ/core/views.py
@@ -6,7 +6,7 @@ from django.views.generic import TemplateView, DetailView
 from django.views import View
 from sympy.integrals.meijerint_doc import category
 from django.db.models import Q
-from core.models import Post, Comment
+from core.models import Post, Category
 
 logger = logging.getLogger(__name__)
 
@@ -14,18 +14,11 @@ logger = logging.getLogger(__name__)
 class HomePageView(TemplateView):
     template_name = 'core/homepage.html'
 
+
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['posts'] = Post.objects.filter(Q(post_status=2)).order_by('-created_at')
         # need some sql or if-then to select posts to show
-        return context
-
-class HomePageEquipmentView(TemplateView):
-    template_name = 'core/homepage.html'
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['posts'] = Post.objects.filter(Q(category1=1)).order_by('-created_at')
         # need some sql or if-then to select posts to show
 
         # context['posts'] = Post.objects.filter(status='published').annotate(
@@ -33,26 +26,88 @@ class HomePageEquipmentView(TemplateView):
         # ).order_by('-score', '-created_at')  # 先按得分降序，再按发布时间降序
         return context
 
-# class HomePageView(TemplateView):
-#     template_name = 'core/homepage.html'
-#
-#     def get_context_data(self, **kwargs):
-#         context = super().get_context_data(**kwargs)
-#         context['posts'] = Post.objects.all().order_by('-created_at')
-#         # need some sql or if-then to select posts to show
-#         return context
-#
-# class HomePageView(TemplateView):
-#     template_name = 'core/homepage.html'
-#
-#     def get_context_data(self, **kwargs):
-#         context = super().get_context_data(**kwargs)
-#         context['posts'] = Post.objects.all().order_by('-created_at')
-#         # need some sql or if-then to select posts to show
-#         return context
-#
+
+class HomePageEquipmentView(TemplateView):
+    template_name = 'core/homepage.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['posts'] = Post.objects.filter(Q(category=1)).order_by('-created_at')
+        return context
+
+class HomeLandscapeView(TemplateView):
+    template_name = 'core/homepage.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['posts'] = Post.objects.filter(Q(category=2)).order_by('-created_at')
+        return context
+
+class HomeNatureView(TemplateView):
+    template_name = 'core/homepage.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['posts'] = Post.objects.filter(Q(category=3)).order_by('-created_at')
+        return context
 
 
+class HomeStreetView(TemplateView):
+    template_name = 'core/homepage.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['posts'] = Post.objects.filter(Q(category=4)).order_by('-created_at')
+        return context
+
+
+class HomeTravelView(TemplateView):
+    template_name = 'core/homepage.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['posts'] = Post.objects.filter(Q(category=5)).order_by('-created_at')
+        return context
+
+
+class HomeLifestyleView(TemplateView):
+    template_name = 'core/homepage.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['posts'] = Post.objects.filter(Q(category=6)).order_by('-created_at')
+        return context
+
+
+class HomeCreativeView(TemplateView):
+    template_name = 'core/homepage.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['posts'] = Post.objects.filter(Q(category=7)).order_by('-created_at')
+        return context
+
+
+class HomeObjectView(TemplateView):
+    template_name = 'core/homepage.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['posts'] = Post.objects.filter(Q(category=8)).order_by('-created_at')
+        return context
+
+
+class HomePortraitureView(TemplateView):
+    template_name = 'core/homepage.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['posts'] = Post.objects.filter(Q(category=9)).order_by('-created_at')
+        return context
+
+
+class AboutUsView(TemplateView):
+    template_name = 'core/about-us.html'
 
 
 
