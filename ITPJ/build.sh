a1e71e3688f9ee19e58ad2a75fc3609d906094fc
HE <2890237931@qq.com>
2025-03-18 20:48:53 +0000

    change

--- ITPJ/build.sh
+++ ITPJ/build.sh
@@ -1,4 +1,4 @@
-pip install -r requirements.txt
+pip install --no-cache-dir --ignore-installed -r requirements.txt || true
 
 python manage.py migrate
 
@@ -6,3 +6,4 @@ if [[ $CREATE_SUPERUSER ]];
 then
   python manage.py createsuperuser --no-input --email "$DJANGO_SUPERUSER_EMAIL"
 fi
+
