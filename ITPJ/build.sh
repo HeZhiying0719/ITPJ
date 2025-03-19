0cc83e54aa9ec59b62ea9bdfe814a8c6e1e155a7
HE <2890237931@qq.com>
2025-03-19 15:45:03 +0000

    admin

--- ITPJ/build.sh
+++ ITPJ/build.sh
@@ -1,6 +1,8 @@
 pip install -r requirements.txt
 
 python manage.py collectstatic --noinput
+find . -path "*/migrations/*.pyc" -delete
+python manage.py makemigrations
 python manage.py migrate
 
 python manage.py createsuperuser --noinput || true
