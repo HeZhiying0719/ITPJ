1abdd7f1b76a2de53419c4fc3e6b0cdcea989e04
HE <2890237931@qq.com>
2025-03-19 13:17:55 +0000

    database

--- ITPJ/ITPJ/settings.py
+++ ITPJ/ITPJ/settings.py
@@ -94,10 +94,10 @@ DATABASES = {
 DATABASES["default"] = dj_database_url.parse("postgresql://focus_hr6e_user:e9PUNOQ1tWp5BY2f2zT0vtNY65K4OSvS@dpg-cvd0pihu0jms739itf3g-a/focus_hr6e")
 
 ALLOWED_HOSTS = [
-    '.onrender.com',   # Render 服务
-    'focus-qni6.onrender.com',  # 你的 Render 域名
-    'localhost',        # 本地测试
-    '127.0.0.1'         # 本地测试
+    '.onrender.com',
+    'focus-qni6.onrender.com',
+    'localhost',
+    '127.0.0.1'
 ]
 
 # Password validation
