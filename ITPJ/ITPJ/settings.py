bbe6c5c72d946ca8c1c7ff22e5d00de8ea1c9122
HE <2890237931@qq.com>
2025-03-20 12:48:20 +0000

    user avatar

--- ITPJ/ITPJ/settings.py
+++ ITPJ/ITPJ/settings.py
@@ -12,6 +12,10 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 import os
 from pathlib import Path
 import dj_database_url
+import cloudinary
+import cloudinary.uploader
+import cloudinary.api
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -165,8 +169,6 @@ if 'RENDER' in os.environ:
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-
-
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
